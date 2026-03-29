9461d038e1483f36c5a020cddcdbd384c420b1d8
bsse25024 <bsse25024@itu.edu.pk>
2026-03-29 21:39:22 +0500

    lab 6 ++

--- functions.cpp
+++ functions.cpp
@@ -3,10 +3,22 @@
 
 int sumOfSquares(int n) 
 {
+    // Base case
+    if (n == 0) {
+        return 0;
+    }
+    // Recursive step
+    return (n * n) + sumOfSquares(n - 1);
 }
 
 int findFactorial(int n)
 {
+    // Base case
+    if (n == 0 or n == 1) {
+        return 1;
+    }
+    // Recursive step
+    return n * findFactorial(n - 1);
 }
 
 
--- main.cpp
+++ main.cpp
@@ -2,5 +2,41 @@
 
 
 int main() {
+    int choice;
+    int num;
+    do {
+        // Displaying the menu
+        cout << "========== MENU ==========" << endl;
+        cout << "1. Find Sum of Squares " << endl;
+        cout << "2. Find Factorial 3" << endl;
+        cout << "3. Exit" << endl;
+        cout << "==========================" << endl;
+        cout << "Enter your choice (1-3): ";
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                cout << " Task A " << endl;
+                cout << "Enter a number: ";
+                cin >> num;
+                cout << "Sum of squares: " << sumOfSquares(num) << endl;
+                break;
+
+            case 2:
+                cout << " Task B " << endl;
+                cout << "Enter a number: ";
+                cin >> num;
+                cout << "Factorial of the number is " << findFactorial(num) << endl;
+                break;
+
+            case 3:
+                cout << "Exiting the program. Goodbye!" << endl;
+                break;
+
+            default:
+                // Handle invalid input
+                cout << "Invalid choice! Please enter 1, 2, or 3." << endl;
+        }
+    } while (choice != 3);
+    return 0;
    
 }
